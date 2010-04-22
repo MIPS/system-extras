4f2835223e52e8063ad587cde372b76a395f684a
Chris Dearman <chris@mips.com>
2010-04-22 10:58:44 -0700

    Enable tests for MIPS

--- tests/Android.mk
+++ tests/Android.mk
@@ -1,3 +1,10 @@
+LOCAL_BUILD:=false
 ifeq ($(TARGET_ARCH),arm)
+LOCAL_BUILD:=true
+endif
+ifeq ($(TARGET_ARCH),mips)
+LOCAL_BUILD:=true
+endif
+ifeq ($LOCAL_BUILD,true)
 include $(call all-subdir-makefiles)
 endif
